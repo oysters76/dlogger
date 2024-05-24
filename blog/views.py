fd52076ccc2fbe8dbe03725b57cd8f27a87badd2
Chirath <chirath.nishshanka@gmail.com>
2024-05-24 13:55:55 +0530

    Made users app

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,9 @@
 from django.shortcuts import render 
-
-posts = [
-    {
-        'author': 'CoreyMs', 
-        'title': 'blog post 1', 
-        'content': 'first content post', 
-        'date_posted': 'August 26, 2029'
-    },
-     {
-        'author': 'Jane done', 
-        'title': 'blog post 2', 
-        'content': 'second content post', 
-        'date_posted': 'August 27, 2029'
-    },
-     {
-        'author': 'Broseft', 
-        'title': 'blog post 3', 
-        'content': 'third content post', 
-        'date_posted': 'August 28, 2029'
-    }
-]
+from .models import Post 
 
 # Create your views here.
 def home(request):
-    context = {'posts': posts, 'title':'Home!'}
+    context = {'posts': Post.objects.all(), 'title':'Home!'}
     return render(request, 'blog/home.html', context)
 
 def about(request):
