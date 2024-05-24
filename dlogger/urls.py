c17aebafe264e85e6f8a00e62358309d14bdcf18
Chirath <chirath.nishshanka@gmail.com>
2024-05-24 16:48:20 +0530

    Add user profile page

--- dlogger/urls.py
+++ dlogger/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path,include 
 from django.contrib.auth import views as auth_views 
 from users import views as users_view 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +12,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'), 
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'), 
     path('profile/', users_view.profile, name='user-profile')
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
